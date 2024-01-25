a2f5c5f94538fd72c4569b0fe38b79644ac7ebf5
Bouslah Taha <bouslahtaha33@gmail.com>
2024-01-25 14:59:53 +1300

    add default styles

--- skills-front-end-vuejs/src/App.vue
+++ skills-front-end-vuejs/src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
+    <div class="max-w-7xl mx-auto">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -12,5 +12,7 @@ import { RouterLink, RouterView } from 'vue-router'
     </div>
   </header>
 
-  <RouterView />
+  <main class="max-w-7xl mx-auto min-h-screen" >
+    <RouterView />
+  </main> 
 </template>
