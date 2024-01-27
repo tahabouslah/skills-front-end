5ce923c4cc837eb05d7427208d806a9bf475f082
Bouslah Taha <bouslahtaha33@gmail.com>
2024-01-27 14:27:09 +1300

    use the Laravel Api

---
+++ skills-front-end-vuejs/src/composables/skills.js
@@ -0,0 +1,64 @@
+
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1';
+
+export default function useSkills() {
+    
+    const skills = ref([]);
+    const skill = ref([]);
+    const errors = ref([]);
+    const router = useRouter();
+
+    const getSkills = async () => {
+        const response = await axios.get("skills");
+        skills.value = response.data.data
+    };
+
+    const getSkill = async (id) => {
+        const response = await axios.get('skill' + id);
+        skill.value = response.data.data;
+    }
+
+    const storeSkill = async(data) => {
+        try {
+            await axios.post('skills', data);
+            await router.push({name: "skillIndex"})
+        }catch (error) {
+            if(error.response.status === 422) {
+                errors.value = error.response.data.errors;
+            }
+        }
+    }
+
+    const updateSkill = async(id) => {
+        try {
+            await axios.put('skills/' + id, skill.value);
+            await router.push({name: "skillIndex"})
+
+        }catch(error){
+            if(error.response.status === 422) {
+                errors.value = error.response.data.errors;
+            }
+        }
+    }
+
+    const destroySkill = async(id) => {
+        try {
+            if(window.confirm("Are you Sure ?")) {
+                return;    
+            }
+            await axios.delete('skills/' + id);
+            await getSkills();
+
+        }catch(error){
+            if(error.response.status === 422) {
+                errors.value = error.response.data.errors;
+            }
+        }
+    }
+
+    return {skills, skill, getSkill,getSkills, updateSkill, destroySkill, errors };
+}
--- skills-front-end-vuejs/src/views/skills/SkillIndex.vue
+++ skills-front-end-vuejs/src/views/skills/SkillIndex.vue
@@ -1,4 +1,43 @@
-<script setup></script>
+<script setup>
+    import useSkills from '../../composables/skills';
+    import { onMounted } from 'vue';
+
+    const {skills, getSkills} = useSkills();
+
+    onMounted(() => getSkills());
+
+</script>
 <template>
-    
+    <div class="mt-12">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Slug
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="skill in skills" :key="skill.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{skill.name}}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{skill.slug}}
+                        </td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
